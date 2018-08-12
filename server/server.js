ad2f6a9072eff8dc633ead908f635cf828988216
Ravish <ravish4596@gmail.com>
2018-08-12 23:47:44 +0530

    Add DELETE /todos route

--- server/server.js
+++ server/server.js
@@ -33,16 +33,37 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
     if(!todo) {
       return res.status(404).send('ID Not Found');
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send(e);
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send('Id not found');
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
