95c870ec9a9259bbe1d2ba087c1f8618be7ee84d
Ravish <ravish4596@gmail.com>
2018-08-12 23:03:41 +0530

    Just a modify

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started app on port ${port}`);
 });
 
 module.exports = {app};
